e9f0d23b06ba7623747d9e58dc33758cc47fa525
C. Vic Hu <vic@cvhu.org>
2013-04-11 14:41:38 -0500

    rails new with survey model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quicksurvey::Application.config.secret_token = '8eb869f2a22f06374a762a7dc223d3df44e37d11609dd24c31de32b97524fed00e7e359b8bb4da25d7530948c8ec8e7fe80dc24fa27d8d93f97e2c6c8072f869'
